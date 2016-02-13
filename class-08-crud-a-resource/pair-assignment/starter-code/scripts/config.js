2d53ea8195a58a6940be3b5a51d164e0f512f835
FazioD <faziod@uw.edu>
2016-02-13 09:17:24 -0800

    update to server

---
+++ class-08-crud-a-resource/pair-assignment/starter-code/scripts/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB: process.env.DB || 'postgres://mubvnohx:6pR_3oeocPOc2NdpmEWXpww-2u26XciM@pellefant.db.elephantsql.com:5432/mubvnohx',
+  PORT: Number(process.env.PORT) || 3000
+}
